b0e114c418e799eeecce72f1e8c30db232ec5a76
h2ljh <h2ljh@naver.com>
2020-08-30 21:08:09 +0900

    modify

--- Java_HomeWork_005/src/com/callor/pay/service/LjhService.java
+++ Java_HomeWork_005/src/com/callor/pay/service/LjhService.java
@@ -6,12 +6,12 @@ package com.callor.pay.service;
 
 public class LjhService 
 {
-	private final int[] money = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
-	private int[] moneyList;
+	private final int[] moneyArr = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
+	private int[] moneyListArr;
 	
 	public LjhService() 
 	{
-		moneyList = new int[money.length];
+		moneyListArr = new int[moneyArr.length];
 	}
 		
 	public void solution(int pay) 
@@ -20,22 +20,22 @@ public class LjhService
 		int temp = 0;
 		temp = (pay / 10) * 10;
 		
-		for(int i=0; i <money.length; ++i)
+		for(int i=0; i <moneyArr.length; ++i)
 		{
 			if(temp <= 0)
 			   break;
 			
-			ans = temp / money[i];
-			moneyList[i] = ans;
-			temp %= money[i];
+			ans = temp / moneyArr[i];
+			moneyListArr[i] = ans;
+			temp %= moneyArr[i];
 		}
 	}
 	
 	public void list()
 	{
 		System.out.println("=====================");
-		for(int i=0; i<moneyList.length; ++i)
-			System.out.printf("%d원 권 : %d\n", money[i], moneyList[i]);
+		for(int i=0; i<moneyListArr.length; ++i)
+			System.out.printf("%d원 권 : %d\n", moneyArr[i], moneyListArr[i]);
 		System.out.println("=====================");
 	}
 	
